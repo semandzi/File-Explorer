1f803e1bb06d7042f6949e0ec8c872a8302be712
Senad Mandžić <semandzi@vsite.hr>
2020-02-20 18:03:06 +0100

    Add project files.

---
+++ File Explorer/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnBackward.Location = new System.Drawing.Point(2, 12);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(34, 31);
+            this.btnBackward.TabIndex = 0;
+            this.btnBackward.Text = "<<";
+            this.btnBackward.UseVisualStyleBackColor = true;
+            this.btnBackward.Click += new System.EventHandler(this.btnBackward_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnForward.Location = new System.Drawing.Point(42, 12);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(35, 31);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = ">>";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnOpen.Location = new System.Drawing.Point(683, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(90, 31);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(83, 18);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(41, 17);
+            this.lblPath.TabIndex = 3;
+            this.lblPath.Text = "Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(130, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(537, 22);
+            this.txtPath.TabIndex = 4;
+            this.txtPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // webBrowser
+            // 
+            this.browser.Location = new System.Drawing.Point(130, 81);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "webBrowser";
+            this.browser.Size = new System.Drawing.Size(537, 319);
+            this.browser.TabIndex = 5;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBackward);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "File Explorer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
+
---
+++ File Explorer/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select you're path" }) {
+                if (fbd.ShowDialog() == DialogResult.OK)
+            
+                    browser.Url=new Uri(fbd.SelectedPath);
+                txtPath.Text = fbd.SelectedPath;
+
+            }
+        }
+
+
+        private void btnBackward_Click(object sender, EventArgs e)
+        {
+            if (browser.CanGoBack)
+                browser.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (browser.CanGoForward)
+                browser.GoForward();
+        }
+
+        
+    }
+}
